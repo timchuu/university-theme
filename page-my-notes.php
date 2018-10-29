79fabf69db816df36309c076546314617b437e6d
timchuu <40304454+timchuu@users.noreply.github.com>
2018-10-29 16:33:09 -0700

    display and create, delete post from front end using wordpress rest api

--- page-my-notes.php
+++ page-my-notes.php
@@ -13,6 +13,16 @@ while(have_posts()) {
 	
 
   <div class="container container--narrow page-section">
+
+    <div class="create-note">
+      
+      <h2 class="headline headline--medium">Create New Note</h2>
+
+      <input class="new-note-title" placeholder="Title"type="" name="">
+      <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+      <span class="submit-note">Create Note</span>
+
+    </div>
     <ul class="min-list link-list" id="my-notes">
       <?php 
       $userNotes = new WP_Query(array(
@@ -24,10 +34,11 @@ while(have_posts()) {
       while($userNotes->have_posts()) {
         $userNotes->the_post(); ?>
         <li data-id="<?php the_ID();?>">
-          <input value="<?php echo esc_attr(get_the_title());?>" class="note-title-field">
+          <input readonly value="<?php echo esc_attr(get_the_title());?>" class="note-title-field">
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-          <textarea class="note-body-field"><?php echo esc_attr(get_the_content());?></textarea>
+          <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content());?></textarea>
+          <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
         </li>
       <?php }
 
